15c68f4b775bcda1f67a7ee7b6bbb54db0d037e8
Alice Yuan <alice.y@myplanetdigital.com>
2012-07-26 08:21:37 -0400

    adding icons & further styling

--- website3/js/global.js
+++ website3/js/global.js
@@ -8,23 +8,36 @@ $(document).ready(function(){
   }
   */
 
+  var $default_circle_size = "30px";
+  var $hover_circle_size = "40px";
 
-  $(".circle-inner").mouseover(function(){
-    if (!$(this).hasClass("active")) {
-      $(this).stop().animate({
-        "width": "26px",
-        "height":"26px",
-        "margin-top":"-3px",
-        },300,'swing');
-      }
-    }).mouseout(function(){
+/*
+  $(".nav li").mouseover(function(){
+    $(this).find("p").text("it works!");
+  });
+  */
+  $(".main-menu .nav li").mouseover(function(){
+    $(this).find(".circle-inner").(function(){
       if (!$(this).hasClass("active")) {
         $(this).stop().animate({
-          "width": "20px",
-          "height": "20px",
+          "width": $hover_circle_size,
+          "height": $hover_circle_size,
+          "margin-top":"-5px",
+          },300,'swing');
+      }
+    });
+  })mouseout.(function(){
+      $(this).addClass("hover");
+      if (!$(this).hasClass("active")) {
+        $(this).stop().animate({
+          "width": $default_circle_size,
+          "height": $default_circle_size,
           "margin-top":"0px",
           },250,'swing');
+
       }
     });
  });
+
+});
 
