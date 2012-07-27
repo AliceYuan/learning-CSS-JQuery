64a52b049c180ee6fd062cde401cb644a5540a8e
AliceYuan <alice.y@myplanetdigital.com>
2012-07-26 23:00:15 -0400

    adding responsive design aspects

--- website3/js/global.js
+++ website3/js/global.js
@@ -1,43 +1,31 @@
 $(document).ready(function(){
 
-/*
-  $(".circle").mouseover(function(){
-    $('.circle').transition({ scale: 1.2 });
-  }.mouseout(function(){
-    $('.circle').transition({ scale: 1 });
+  if ($(window).height() > $(window).width()){
+    $(".main-menu-wrapper").addClass("top").removeClass("right");
+    $(".main-menu-wrapper .main-menu .nav").css({
+      "padding": "0 "+$(window).width()/10+"px",
+    });
+  }else{
+    $(".main-menu-wrapper").addClass("right").removeClass("top");
+    $(".main-menu-wrapper .main-menu .nav").css({
+      "padding": $(window).height()/11+"px 0 ",
+    });
   }
-  */
 
   var $default_circle_size = "30px";
   var $hover_circle_size = "40px";
-
-/*
-  $(".nav li").mouseover(function(){
-    $(this).find("p").text("it works!");
+  $(window).resize(function() {
+    if ($(window).height() > $(window).width()){
+      $(".main-menu-wrapper").addClass("top").removeClass("right");
+      $(".main-menu-wrapper .main-menu .nav").css({
+        "padding": "0 "+$(window).width()/10+"px",
+      });
+    }else{
+      $(".main-menu-wrapper").addClass("right").removeClass("top");
+      $(".main-menu-wrapper .main-menu .nav").css({
+        "padding": $(window).height()/11+"px 0 ",
+      });
+    }
   });
-  */
-  $(".main-menu .nav li").mouseover(function(){
-    $(this).find(".circle-inner").(function(){
-      if (!$(this).hasClass("active")) {
-        $(this).stop().animate({
-          "width": $hover_circle_size,
-          "height": $hover_circle_size,
-          "margin-top":"-5px",
-          },300,'swing');
-      }
-    });
-  })mouseout.(function(){
-      $(this).addClass("hover");
-      if (!$(this).hasClass("active")) {
-        $(this).stop().animate({
-          "width": $default_circle_size,
-          "height": $default_circle_size,
-          "margin-top":"0px",
-          },250,'swing');
-
-      }
-    });
- });
-
 });
 
