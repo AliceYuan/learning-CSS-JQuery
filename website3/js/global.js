04a0272d972f901d08094f65c3adf1de7940e61c
AliceYuan <alice.y@myplanetdigital.com>
2012-08-20 19:30:28 -0400

    file renaming, front page redesign and blog page styling

--- website3/js/global.js
+++ website3/js/global.js
@@ -2,76 +2,19 @@ $(document).ready(function(){
   var default_button_size = '30px';
   var hover_button_size = '40px';
   resize();
+  $('.scrollTop a').click(function(){
+    $('html, body').animate({ scrollTop: 0 }, 'slow');
+  });
   $(window).resize(function() {
     resize();
   });
   windowLoad();
 });
 
-function addClasses(){
-  var windowW = $(window).width();
-  var windowH = $(window).height();
-  //add portrait / landscape class
-  if (windowH > windowW){
-    $('body').addClass('portrait').removeClass('landscape');
-  }else{
-    $('body').addClass('landscape').removeClass('portrait');
-  }
-
-  //remove previous screen size classes
-  $('body').removeClass (function (index, css) {
-      return (css.match (/\bscreen-width-\S+/g) || []).join(' ');
-  });
-  $('body').removeClass (function (index, css) {
-      return (css.match (/\bscreen-height-\S+/g) || []).join(' ');
-  });
-
-  //add width of screen class
-  if (windowW > 1023){
-    $('body').addClass('screen-width-large');
-  }else if (windowW > 767){
-    $('body').addClass('screen-width-medium');
-  }else if (windowW > 479){
-    $('body').addClass('screen-width-small');
-  }else{
-    $('body').addClass('screen-width-xsmall');
-  }
-
-  //add height of screen class
-  if (windowH > 1023){
-    $('body').addClass('screen-height-large');
-  }else if (windowH > 767){
-    $('body').addClass('screen-height-medium');
-  }else if (windowH > 479){
-    $('body').addClass('screen-height-small');
-  }else{
-    $('body').addClass('screen-height-xsmall');
-  }
-}
-
 function windowLoad(){
   $(window).load( function() {
-    centerIcons();
-    // resize();
+    resize();
   });
-}
-
-function centerIcons(){
-  // var liSize = $('.main-menu .nav li').size();
-
-  // var liW = $('.main-menu ul li').width();
-  // var liW = $('.main-menu ul li').height();
-  // $('.main-menu ul li').each( function($index) {
-  //   var buttonSize = $(this).find('.button-inner').width();
-  //   $(this).find('img.icon').load( function () {
-  //     var iconW = (buttonSize/$(this).width()*0.52)*$(this).width();
-  //     var iconH = (buttonSize/$(this).height()*0.52)*$(this).height();
-  //     $(this).css({
-  //       'width': iconW+'px',
-  //       'height': iconH+'px',
-  //     });
-  //   });
-  // });
 }
 
 
@@ -97,31 +40,37 @@ function resizeTitle(){
   // equalHeightWidth($title);
 }
 
-function resize(){
-  addClasses();
-  centerIcons();
-  resizeTitle();
-  // $('.main-menu').height($('.main-menu-wrapper').height());
+function centerMain(){
   var mainW = $('.main-wrapper').width();
+  var windowW = $(window).width();
   var mainH = $('.main-wrapper').height();
-  var mainMR = parseInt($('.main-wrapper').css('margin-right'));
+  var mainML = parseInt($('.main-wrapper').css('margin-left'));
   var mainMT = parseInt($('.main-wrapper').css('margin-top'));
-  if (mainMR == 0 && mainMT == 0){
-    $('.main').css({
-      'height': '100%',
+  if (mainML > 0 && mainMT == 0){
+      $('.main-wrapper').css({
+      'width': (windowW - mainML)+'px',
+    });
+  }else{
+    $('.main-wrapper').css({
       'width': '100%',
     });
-  } else if (mainMR == 0){
-    $('.main').css({
-      'height': '100%',
-      'width': '100%',
-    });
-  }else if(mainMT == 0) {
-    $('.main').css({
-    'width': (mainW - mainMR)+'px',
-    'height': '100%',
-  });
   }
+  //   $('.main').css({
+  //     'height': '100%',
+  //     'width': '100%',
+  //   });
+  // if(mainMT == 0) {
+  //   $('.main-wrapper').css({
+  //   'width': (mainW - mainMR)+'px',
+  //   'height': '100%',
+  // });
+  // } else {
+}
+
+function resize(){
+  centerMain();
+  resizeTitle();
+  blog();
 }
 
 //function makes height the same as the width of an object
@@ -134,6 +83,22 @@ function centerVertical($object, $parentObject){
   var parentH = $parentObject.height();
   var objectH = $object.height();
   $object.css({
-    'margin': ((parentH - objectH)/2)+'px',
+    'margin-top': ((parentH - objectH)/2)+'px',
   });
+}
+
+
+function centerHorizontal($object, $parentObject){
+  var parentW = $parentObject.width();
+  var objectW = $object.width();
+  $object.css({
+    'margin-left': ((parentW - objectW)/2)+'px',
+  });
+}
+//javascript functions for blog page
+function blog(){
+  if ($('body').hasClass('blog')){
+    centerHorizontal($('.filters .categories'), $('.filters'));
+
+  }
 }
